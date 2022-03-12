f1f68937f9c008305cf4186d343845eabc9d8f2f
Leonardo Mello <leonardomello2100@gmail.com>
2022-03-12 14:25:31 -0300

    mostrando na tableView

--- GithubRepo/ViewController.swift
+++ GithubRepo/ViewController.swift
@@ -7,15 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+
+@IBOutlet weak var repoTableView: UITableView!
+
+var fetchedRepo = [Repositorio]()
 
 override func viewDidLoad() {
 super.viewDidLoad()
 
+repoTableView.dataSource = self
+
 parseData()
 }
 
+override var prefersStatusBarHidden: Bool {
+    return true
+}
+
+func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return fetchedRepo.count
+}
+
+func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    let cell = repoTableView.dequeueReusableCell(withIdentifier: "cell") as! RepositorioTableViewCell
+    
+//    cell?.textLabel?.text = fetchedRepo[indexPath.row].login
+    
+    let repositorio = fetchedRepo[indexPath.row]
+    
+    cell.nome_autor?.text = repositorio.login
+    cell.nome_repositorio?.text = repositorio.name
+    cell.quantidade_estrelas?.text = NumberFormatter().string(from: NSNumber(value: repositorio.stargazers_count))
+    cell.foto_autor.loadFrom(URLAddress: repositorio.avatar_url)
+    
+    return cell
+    
+}
+
 func parseData(){
+
+fetchedRepo = [Repositorio]()
 
 let url = "https://api.github.com/search/repositories?q=language:swift&sort=stars"
 var request = URLRequest(url: URL(string: url)!)
@@ -24,7 +57,7 @@ request.httpMethod = "GET"
 let configuration = URLSessionConfiguration.default
 let session = URLSession(configuration: configuration, delegate: nil, delegateQueue: OperationQueue.main)
 
-let task = session.dataTask(with: request) { (data ,response , error) in
+let task = session.dataTask(with: request) { [self] (data ,response , error) in
 
 if (error != nil) {
 print("Error")
@@ -32,33 +65,34 @@ print("Error")
 
 do {
 let fetchedData = try JSONSerialization.jsonObject(with: data!, options: .mutableLeaves)
+var nome_owner: String
+var foto_owner: String
+var nome_repo: String
+var qntd_estrelas: Int
 
 if let fetchedData = fetchedData as? Dictionary<String, AnyObject>, let repo = fetchedData["items"] as? [Any] {
-    
-        for eachFetchedRepositorio in repo {
-            
-            //pegando o owner
-            if let eachFetchedRepositorio = eachFetchedRepositorio as? AnyObject, let owner = eachFetchedRepositorio["owner"] as? NSDictionary {
-    
-                let nome_owner = owner["login"] as! String
-                let foto_owner = owner["avatar_url"] as! String
-                print(nome_owner)
-                print(foto_owner)
-                
-            }
-            //fim do owner
-            
-        let eachRepositorio = eachFetchedRepositorio as! [String : Any]
-            
-        let nome_repo = eachRepositorio["name"] as! String
-        let qntd_estrelas = eachRepositorio["stargazers_count"] as! Int
-            
-            print(nome_repo)
-            print(qntd_estrelas)
-            print(" ")
-        }
-    
-    
+
+for eachFetchedRepositorio in repo {
+
+//pegando o owner
+if let eachFetchedRepositorio = eachFetchedRepositorio as? AnyObject, let owner = eachFetchedRepositorio["owner"] as? NSDictionary {
+
+nome_owner = owner["login"] as! String
+foto_owner = owner["avatar_url"] as! String
+//fim do owner
+
+let eachRepositorio = eachFetchedRepositorio as! [String : Any]
+
+nome_repo = eachRepositorio["name"] as! String
+qntd_estrelas = eachRepositorio["stargazers_count"] as! Int
+
+self.fetchedRepo.append(Repositorio(name: nome_repo, login: nome_owner, avatar_url: foto_owner, stargazers_count: qntd_estrelas))
+
+}
+
+self.repoTableView.reloadData()
+}
+
 }
 
 } catch {
@@ -72,4 +106,50 @@ task.resume()
 }
 
 
+}
+
+
+class Repositorio {
+
+var name : String
+var login: String
+var avatar_url: String
+var stargazers_count: Int
+
+init(name: String, login: String, avatar_url: String, stargazers_count: Int){
+self.name = name
+self.login = login
+self.avatar_url = avatar_url
+self.stargazers_count = stargazers_count
+}
+
+}
+
+class RepositorioTableViewCell: UITableViewCell {
+
+@IBOutlet weak var foto_autor: UIImageView!
+
+@IBOutlet weak var nome_repositorio: UILabel!
+
+@IBOutlet weak var quantidade_estrelas: UILabel!
+
+@IBOutlet weak var nome_autor: UILabel!
+
+}
+
+
+extension UIImageView {
+    func loadFrom(URLAddress: String) {
+        guard let url = URL(string: URLAddress) else {
+            return
+        }
+        
+        DispatchQueue.main.async { [weak self] in
+            if let imageData = try? Data(contentsOf: url) {
+                if let loadedImage = UIImage(data: imageData) {
+                        self?.image = loadedImage
+                }
+            }
+        }
+    }
 }
